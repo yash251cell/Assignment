0796221622664d795ec5490c456103f0d731f29e
yash251cell <yash.m.chandarana@gmail.com>
2022-01-04 10:10:14 +0530

    Adding new Program

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int sum = num1+num2;
+        System.out.println(sum);
+    }
+}
